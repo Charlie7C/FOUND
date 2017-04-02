908f12a5786482fb38e64ded6e2ca10587d5752e
Charlie7C <ch7x7hc@gmail.com>
2017-04-02 14:20:27 +1200

    New Framework 7 Draft

---
+++ js/my-app.js
@@ -0,0 +1,7 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Init slider and store its instance in mySwiper variable
+var mySwiper = myApp.swiper('.swiper-container', {
+  pagination:'.swiper-pagination'
+});
