2b95e6fa8b85dd0b03c896a5524afe2357e9043b
Charlie7C <ch7x7hc@gmail.com>
2017-04-03 18:52:10 +1200

    new update

--- js/my-app.js
+++ js/my-app.js
@@ -1,7 +1,16 @@
-// Initialize your app
-var myApp = new Framework7();
 
-// Init slider and store its instance in mySwiper variable
-var mySwiper = myApp.swiper('.swiper-container', {
-  pagination:'.swiper-pagination'
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  // Initialize your app
+  var myApp = new Framework7();
+
+  // Init slider and store its instance in mySwiper variable
+  var mySwiper = myApp.swiper('.swiper-container', {
+    pagination:'.swiper-pagination'
+  });
+
+
 });
